dc7f3afcc864d2fd7457d3d99a38e082225b0914
Mau Aguero <mam.121002@gmail.com>
2024-05-11 15:23:30 -0600

    funcion para crear tar

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <bits/getopt_core.h>
 
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libtar.h>
+
 void create_tar(char* tar_filename, char* files[], int num_files);
 void extract_tar(char* tar_filename);
 void list_tar(char* tar_filename);
@@ -12,6 +17,8 @@ void update_tar(char* tar_filename, char* file_to_update);
 void pack_tar(char* tar_filename);
 
 int main(int argc, char *argv[]) {
+    // printf("Comando STAR: %d\n", argc);
+    
     char* tar_filename = NULL;
     char* files[argc];
     int num_files = 0;
@@ -24,6 +31,8 @@ int main(int argc, char *argv[]) {
                 tar_filename = argv[optind];
                 for (int i = optind + 1; i < argc; i++) {
                     files[num_files++] = argv[i];
+                    // printf("FILE %d: %s\n", num_files-1, files[num_files-1]);
+                    // printf("FILE %d (%d): %s | %s\n", num_files-1, i, argv[i], files[num_files-1]);
                 }
                 create_tar(tar_filename, files, num_files);
                 break;
@@ -55,7 +64,36 @@ int main(int argc, char *argv[]) {
 }
 
 void create_tar(char* tar_filename, char* files[], int num_files) {
-    // Implementar la creación de un archivo TAR
+    // Create a tar file
+    int tar_fd = open(tar_filename, O_WRONLY | O_CREAT, 0644);
+    if (tar_fd < 0) {
+        fprintf(stderr, "Error creating tar file '%s': %s\n", tar_filename, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize tar
+    TAR *tar = NULL;
+    if (tar_fdopen(&tar, tar_fd, NULL, NULL, O_WRONLY, 0644, TAR_GNU) != 0) {
+        fprintf(stderr, "Error opening tar archive: %s\n", strerror(errno));
+        close(tar_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Add each file to the tar
+    for (int i = 0; i < num_files; i++) {
+        if (tar_append_file(tar, files[i], NULL) < 0) {
+            fprintf(stderr, "Error adding file '%s' to tar: %s\n", files[i], strerror(errno));
+            tar_close(tar);
+            close(tar_fd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Close the tar file
+    tar_close(tar);
+    close(tar_fd);
+
+    printf("Tar file '%s' created successfully containing the specified files\n", tar_filename);
 }
 
 void extract_tar(char* tar_filename) {
