fdaf65c64b0277bb93d3df667637d4e5931907c2
Gustavo <gperezb2002@gmail.com>
2024-05-13 20:18:40 -0600

    Arreglo FAT

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 #define FAT_ENTRY_SIZE 256 // Tamaño de cada entrada FAT (puede ajustarse según las necesidades)
 #define MAX_BLOCKS 1000 // Número máximo de bloques en el archivo TAR
-#define TAR_BLOCKSIZE 262144
+#define TAR_BLOCKSIZE (256 * 1024)
 
 typedef struct {
     char filename[100]; // Nombre del archivo
@@ -22,54 +22,72 @@ void create_tar(char* tar_filename);
 void extract_tar(char* tar_filename);
 void list_tar(char* tar_filename);
 void delete_from_tar(char* tar_filename, char* file_to_delete);
-void update_tar(char* tar_filename, char* file_to_update);
+void update_tar(char* tar_filename, char** files_to_update);
 void append_to_tar(char* tar_filename, char* files[], int num_files);
 void pack_tar(char* tar_filename);
 void write_header(int tar_fd, const char *filename, off_t size);
 void write_file(int tar_fd, const char *filename, FatEntry *fat_entry);
-void write_fat_entries(int tar_fd, FatEntry *fat_entries, int num_entries, off_t total_blocks);
+void write_fat_entries(int tar_fd, FatEntry *fat_entries, int num_entries);
+int calculate_file_blocks(const char* filename);
 
 int main(int argc, char *argv[]) {
-    // printf("Comando STAR: %d\n", argc);
-    
     char* tar_filename = NULL;
     char* files[argc];
     int num_files = 0;
+    
+    // Variables para controlar qué opciones se han encontrado
+    int c_flag = 0;
+    int x_flag = 0;
+    int u_flag = 0;
+    int f_flag = 0;
     
     int opt;
     while ((opt = getopt(argc, argv, "cxtruf:p")) != -1) {
         switch (opt) {
             case 'c':
-                tar_filename = argv[optind];
-                create_tar(tar_filename);
+                c_flag = 1;
                 break;
             case 'x':
-                extract_tar(argv[optind]);
+                x_flag = 1;
                 break;
             case 't':
-                list_tar(argv[optind]);
+                // Implementar función para el flag -t
                 break;
             case 'r':
                 // Implementar función para el flag -r
                 break;
             case 'u':
-                // Implementar función para el flag -u
+                u_flag = 1;
                 break;
             case 'f':
-                tar_filename = optarg;
-                for (int i = optind; i < argc; i++) {
-                    files[num_files++] = argv[i];
-                }
-                append_to_tar(tar_filename, files, num_files);
+                f_flag = 1;
                 break;
             case 'p':
-                pack_tar(argv[optind]);
+                // Implementar función para el flag -p
                 break;
             default:
                 fprintf(stderr, "Uso: %s [-cxtur] [-f archivo_tar] [-p] [archivo...]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
+        tar_filename = optarg;
     }
+    
+    // Si se encontró el flag -f, procesar los archivos
+    if (f_flag) {
+        for (int i = optind; i < argc; i++) {
+            files[num_files++] = argv[i];
+        }
+    }
+    
+    // Llamar a las funciones en el orden en que se encontraron los flags
+    if (c_flag)
+        create_tar(tar_filename);
+    if (x_flag)
+        extract_tar(tar_filename);
+    if (u_flag)
+        update_tar(tar_filename, files);
+    if (f_flag)
+        append_to_tar(tar_filename, files, num_files);
 
     return 0;
 }
@@ -98,36 +116,50 @@ void delete_from_tar(char* tar_filename, char* file_to_delete) {
     // Implementar la eliminación de un archivo de un archivo TAR
 }
 
-void update_tar(char* tar_filename, char* file_to_update) {
+void update_tar(char* tar_filename, char** file_to_update) {
     // Implementar la actualización de un archivo en un archivo TAR
 }
 
 void append_to_tar(char* tar_filename, char* files[], int num_files) {
-    // Abre el archivo tar para agregar archivos
-    int tar_fd = open(tar_filename, O_WRONLY | O_APPEND);
+    int tar_fd = open(tar_filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (tar_fd < 0) {
-        fprintf(stderr, "Error abriendo el archivo TAR '%s': %s\n", tar_filename, strerror(errno));
+        fprintf(stderr, "Error opening TAR file '%s': %s\n", tar_filename, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    // Crear la estructura FAT para los nuevos archivos
-    FatEntry fat_entries[num_files];
-    off_t current_offset = lseek(tar_fd, 0, SEEK_END);
+    // Calculate total blocks needed for all files
     off_t total_blocks = 0;
-
-    // Escribir cada archivo en el archivo TAR y registrar su posición en la estructura FAT
     for (int i = 0; i < num_files; i++) {
-        write_file(tar_fd, files[i], &fat_entries[i]);
-        total_blocks += fat_entries[i].block_count;
+        int file_blocks = calculate_file_blocks(files[i]);
+        total_blocks += file_blocks;
     }
 
-    // Escribir la estructura FAT al final del archivo TAR
-    write_fat_entries(tar_fd, fat_entries, num_files, total_blocks);
+    // Check if total blocks exceed the maximum
+    if (total_blocks > MAX_BLOCKS) {
+        fprintf(stderr, "Error: Total blocks (%lld) exceed maximum allowed (%d)\n", total_blocks, MAX_BLOCKS);
+        exit(EXIT_FAILURE);
+    }
 
-    // Cerrar el archivo TAR
+    // Allocate and initialize FAT entries
+    FatEntry fat_entries[num_files];
+    off_t current_offset = 0; // Offset for the first file block
+    for (int i = 0; i < num_files; i++) {
+        fat_entries[i].block_count = calculate_file_blocks(files[i]);
+        fat_entries[i].block_offset = current_offset;
+        current_offset += fat_entries[i].block_count;
+    }
+
+    // Write FAT entries at the beginning of the TAR archive
+    lseek(tar_fd, 0, SEEK_SET); // Move to the beginning of the file
+    write_fat_entries(tar_fd, fat_entries, num_files);
+
+    // Now append actual file data with headers
+    for (int i = 0; i < num_files; i++) {
+        write_file(tar_fd, files[i], &fat_entries[i]);
+    }
+
     close(tar_fd);
-
-    printf("Archivos agregados al archivo TAR '%s' exitosamente\n", tar_filename);
+    printf("Files added to TAR archive '%s' successfully\n", tar_filename);
 }
 
 void pack_tar(char* tar_filename) {
@@ -143,10 +175,9 @@ void write_header(int tar_fd, const char *filename, off_t size) {
 }
 
 void write_file(int tar_fd, const char *filename, FatEntry *fat_entry) {
-    // Escribir un archivo en el archivo TAR y actualizar la entrada FAT
     int file_fd = open(filename, O_RDONLY);
     if (file_fd < 0) {
-        fprintf(stderr, "Error abriendo el archivo '%s': %s\n", filename, strerror(errno));
+        fprintf(stderr, "Error opening file '%s': %s\n", filename, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
@@ -154,10 +185,8 @@ void write_file(int tar_fd, const char *filename, FatEntry *fat_entry) {
     stat(filename, &st);
     off_t file_size = st.st_size;
 
-    // Escribir el encabezado del archivo
     write_header(tar_fd, filename, file_size);
 
-    // Escribir los datos del archivo
     char buffer[TAR_BLOCKSIZE];
     ssize_t bytes_read;
     off_t total_bytes_written = 0;
@@ -165,12 +194,9 @@ void write_file(int tar_fd, const char *filename, FatEntry *fat_entry) {
     while ((bytes_read = read(file_fd, buffer, TAR_BLOCKSIZE)) > 0) {
         write(tar_fd, buffer, bytes_read);
         total_bytes_written += bytes_read;
-        if (total_bytes_written % TAR_BLOCKSIZE == 0) {
-            block_count++;
-        }
+        block_count++;
     }
 
-    // Escribir los bloques de padding si es necesario
     int padding_size = TAR_BLOCKSIZE - (file_size % TAR_BLOCKSIZE);
     if (padding_size != TAR_BLOCKSIZE) {
         char padding[TAR_BLOCKSIZE];
@@ -179,41 +205,26 @@ void write_file(int tar_fd, const char *filename, FatEntry *fat_entry) {
         block_count++;
     }
 
-    // Actualizar la entrada FAT
-    strncpy(fat_entry->filename, filename, sizeof(fat_entry->filename) - 1);
-    off_t current_offset = lseek(tar_fd, 0, SEEK_CUR);
-    fat_entry->block_offset = current_offset - total_bytes_written - padding_size;
-    fat_entry->block_count = block_count;
-
+    fat_entry->block_count = block_count; // Set actual block count
     close(file_fd);
 }
 
-void write_fat_entries(int tar_fd, FatEntry *fat_entries, int num_entries, off_t total_blocks) {
-    // Escribir las entradas FAT al inicio del archivo TAR
-    off_t current_block = 0;
+
+void write_fat_entries(int tar_fd, FatEntry *fat_entries, int num_entries) {
+    char entry_info[256];
     for (int i = 0; i < num_entries; i++) {
-        while (current_block < fat_entries[i].block_offset / TAR_BLOCKSIZE) {
-            // Escribir un bloque libre
-            char entry_info[256];
-            memset(entry_info, 0, 256);
-            snprintf(entry_info, 256, "-1 0\n");
-            write(tar_fd, entry_info, strlen(entry_info));
-            current_block++;
-        }
-
-        char entry_info[256];
-        memset(entry_info, 0, 256);
-        snprintf(entry_info, 256, "%lld %lld\n", fat_entries[i].block_offset / TAR_BLOCKSIZE, fat_entries[i].block_count);
+        snprintf(entry_info, 256, "%lld %lld\n", fat_entries[i].block_offset, fat_entries[i].block_count);
         write(tar_fd, entry_info, strlen(entry_info));
-        current_block += fat_entries[i].block_count;
+    }
+}
+
+int calculate_file_blocks(const char* filename) {
+    struct stat st;
+    if (stat(filename, &st) == -1) {
+        fprintf(stderr, "Error getting file size for '%s': %s\n", filename, strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
-    while (current_block < total_blocks) {
-        // Escribir un bloque libre
-        char entry_info[256];
-        memset(entry_info, 0, 256);
-        snprintf(entry_info, 256, "-1 0\n");
-        write(tar_fd, entry_info, strlen(entry_info));
-        current_block++;
-    }
+    off_t file_size = st.st_size;
+    return (file_size + TAR_BLOCKSIZE - 1) / TAR_BLOCKSIZE; // Round up for padding
 }
